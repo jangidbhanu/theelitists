e62cd821e4ead5788f03551b507a4a5c77512e1b
jangidbhanu <36641468+jangidbhanu@users.noreply.github.com>
2019-04-28 03:00:39 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
+$(function () {
+    $(document).scroll(function () {
+        var nav = $(".navbar");
+        nav.toggleClass('navbarscrolled',$(this).scrollTop() > nav.height());
+            
+    });
+});
