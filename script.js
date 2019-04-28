f95660c47f7797b4feacb2b3250e4850311e3c33
jangidbhanu <36641468+jangidbhanu@users.noreply.github.com>
2019-04-28 13:37:22 +0530

    responsive part 1

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $(this).scrollTop(0);
 });
 
-$(function () {
-    $(document).scroll(function () {
+$(function() {
+    $(document).scroll(function() {
         var nav = $(".navbar");
-        nav.toggleClass('navbarscrolled',$(this).scrollTop() > nav.height());
-            
+        nav.toggleClass('navbarscrolled', $(this).scrollTop() > nav.height());
+
     });
 });
+
+function menu() {
+    $(".listmenu").toggleClass('menu');
+
+}
