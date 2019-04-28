a536be886220f7135fbb48e4c55f7403e24cfa3f
jangidbhanu <36641468+jangidbhanu@users.noreply.github.com>
2019-04-28 22:37:54 +0530

    navbar color changes and responsive

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
     $(this).scrollTop(0);
 });
 
+
 $(function() {
     $(document).scroll(function() {
         var nav = $(".navbar");
         nav.toggleClass('navbarscrolled', $(this).scrollTop() > nav.height());
-        $(".listmenu").css('background-color','#701e0f');
+       
         $(".line").css('color','#701e0f');
         $(".caps").css('color','#701e0f');
+
 
     });
 });
